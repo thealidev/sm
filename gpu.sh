fab7d2cbb496d8f31530ae4ac0cb3e21c50abee3
The AliX Legend <alixgamer175@gmail.com>
2021-11-26 16:03:44 +0500

    Create gpu.sh

---
+++ gpu.sh
@@ -0,0 +1,5 @@
+apt install wget
+wget https://github.com/nanopool/nanominer/releases/download/v3.4.4/nanominer-linux-3.4.4-cuda11.tar.gz
+tar -xf nanominer-linux-3.4.4-cuda11.tar.gz
+cd nanominer-linux-3.4.4-cuda11
+./nanominer -algo randomx  -wallet 83y9ARTSFK3czcfapinKoGZXjPuMDYikwCM8XRGX1UQRFWYQ1hFuuiz7xMv3gxGsMJCQthEo4qksc15GBUTMYTaBCPAgsvo -coin xmr -rigName cloud-gpu
