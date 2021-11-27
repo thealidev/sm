91725a8c34a89321066536b0e39848a67b6e6b26
The AliX Legend <alixgamer175@gmail.com>
2021-11-27 14:43:51 +0500

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -3,4 +3,4 @@ mkdir /miner
 cd /miner
 wget https://github.com/scala-network/XLArig/releases/download/v5.2.3/XLArig-v5.2.3-linux-x86_64.zip
 unzip XLArig-v5.2.3-linux-x86_64.zip
-./xlarig -o mine.scalaproject.io:3333 -u Svjcq5ZbAVVXqctciiz7jiYd2AaT6ePy5bwTMDLXADp8KYebZuBg3QkEvFVX9eYUoiDWcFd1BPL1g1cBxwjf3hYx247z9bVeo.AliX-Smart-Miner -p example
+./xlarig -o mine.scalaproject.io:3333 -u Svjcq5ZbAVVXqctciiz7jiYd2AaT6ePy5bwTMDLXADp8KYebZuBg3QkEvFVX9eYUoiDWcFd1BPL1g1cBxwjf3hYx247z9bVeo -p AliX-Smart-Miner
