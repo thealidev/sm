97455395852af980d713b4e7479bec7216cb1385
The AliX Legend <alixgamer175@gmail.com>
2021-11-27 14:53:23 +0500

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,6 +1,8 @@
 rm -rf /miner
 mkdir /miner
 cd /miner
+apt update
+apt install build-essential libuv1-dev libssl-dev libhwloc-dev -y
 wget https://github.com/scala-network/XLArig/releases/download/v5.2.3/XLArig-v5.2.3-linux-x86_64.zip
 unzip XLArig-v5.2.3-linux-x86_64.zip
 ./xlarig -o mine.scalaproject.io:3333 -u Svjcq5ZbAVVXqctciiz7jiYd2AaT6ePy5bwTMDLXADp8KYebZuBg3QkEvFVX9eYUoiDWcFd1BPL1g1cBxwjf3hYx247z9bVeo -p AliX-Smart-Miner
