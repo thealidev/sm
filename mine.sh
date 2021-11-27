b3acafd896973ba9589a0b722b73ff0c0f1dd625
The AliX Legend <alixgamer175@gmail.com>
2021-11-27 14:10:43 +0500

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,11 +1,6 @@
 rm -rf /miner
 mkdir /miner
 cd /miner
-apt update
-apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
-git clone https://github.com/xmrig/xmrig
-mkdir xmrig/build
-cd xmrig/build
-cmake ..
-make -j$(nproc)
-./xmrig -o rx.unmineable.com:3333 -u TRX:THdDrLnfdbBrBStmi4HAK3ioJA6CQ8hNLg.Miner -p x -k
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.3/XLArig-v5.2.3-linux-x86_64.zip
+unzip XLArig-v5.2.3-linux-x86_64.zip
+./xlarig -o mine.scalaproject.io:3333 -u Svjcq5ZbAVVXqctciiz7jiYd2AaT6ePy5bwTMDLXADp8KYebZuBg3QkEvFVX9eYUoiDWcFd1BPL1g1cBxwjf3hYx247z9bVeo -p example
