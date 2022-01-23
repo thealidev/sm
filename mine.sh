508f6c03f487732db3e328146e4c09abec4679a3
The AliX Legend <alixgamer175@gmail.com>
2022-01-23 23:19:53 +0500

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -2,9 +2,9 @@ rm -rf ~/miner
 mkdir ~/miner
 cd ~/miner
 apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
-git clone https://github.com/Bendr0id/xmrigCC
-mkdir xmrigCC/build
-cd xmrigCC/build
+git clone https://github.com/xmrig/xmrig
+mkdir xmrig/build
+cd xmrig/build
 cmake ..
 make -j$(nproc)
 ./xmrig --opencl --cuda -o rx.unmineable.com:3333 -u TRX:TP7y2JTBi4AVDPtDigAGDkQr7fySeuJ7Cw.0000 -p x -k
