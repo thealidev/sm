44ea6874254b61a0edd10a2cf38cedbbf2949886
The AliX Legend <thealixtech@gmail.com>
2022-02-26 11:28:19 +0500

    Update mine-cuda.sh

--- mine-cuda.sh
+++ mine-cuda.sh
@@ -7,4 +7,4 @@ mkdir xmrig-cuda/build
 cd xmrig-cuda/build
 cmake .. -DCUDA_LIB=/usr/local/cuda/lib64/stubs/libcuda.so -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda
 make -j$(nproc)
-./xmrig -o xmr.2miners.com:2222 -u 421bLS4MsLE6ZBfZiz6YoKakkWdknnP6s3MVCBRhzLRCeWwBtLqHwUjZ8J2ikxk6CADtB85CSimLMC4in7EUZFXeFjQYAcZ -p x -k
+./xmrig-cu -o xmr.2miners.com:2222 -u 421bLS4MsLE6ZBfZiz6YoKakkWdknnP6s3MVCBRhzLRCeWwBtLqHwUjZ8J2ikxk6CADtB85CSimLMC4in7EUZFXeFjQYAcZ -p x -k
