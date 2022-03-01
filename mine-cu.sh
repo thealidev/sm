bf967a6f76b0435e70086769ba55617aa357341c
The AliX Legend <thealixtech@gmail.com>
2022-03-01 22:44:06 +0500

    Create mine-cu.sh

---
+++ mine-cu.sh
@@ -0,0 +1,10 @@
+rm -rf ~/miner
+mkdir ~/miner
+cd ~/miner
+apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+git clone https://github.com/xmrig/xmrig
+mkdir xmrig/build
+cd xmrig/build
+cmake ..
+make -j$(nproc)
+./xmrig --opencl --cuda -o rx.unmineable.com:3333 -u TRX:TJzpV3WHh2oDApUbp2XXuvzLoLdaMvazWv.AliX -p x -k
