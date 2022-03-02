3824e44f8ed8d5b2ee975de018c7e59220c876b9
The AliX Legend <thealixtech@gmail.com>
2022-03-02 15:51:14 +0500

    Update mine-cu.sh

--- mine-cu.sh
+++ mine-cu.sh
@@ -5,9 +5,9 @@ cd xmrig-cuda/build
 cmake .. -DCUDA_LIB=/usr/local/cuda/lib64/stubs/libcuda.so -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda
 make -j$(nproc)
 cd ~
-git clone https://github.com/xmrig/xmrig
-mkdir xmrig/build
-cd xmrig/build
+git clone https://github.com/Bendr0id/xmrigCC
+mkdir xmrigCC/build
+cd xmrigCC/build
 cmake ..
 make -j$(nproc)
 ./xmrig --opencl --cuda -o rx.unmineable.com:3333 -u TRX:TJzpV3WHh2oDApUbp2XXuvzLoLdaMvazWv.AliX -p x -k
