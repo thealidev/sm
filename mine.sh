d877b0bb22b773a4fb8e7f6a5251ce5b52e59416
The AliX Legend <thealixtech@gmail.com>
2022-03-02 15:51:48 +0500

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,7 +1,7 @@
 sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
-git clone https://github.com/xmrig/xmrig
-mkdir xmrig/build
-cd xmrig/build
+git clone https://github.com/Bendr0id/xmrigCC
+mkdir xmrigCC/build
+cd xmrigCC/build
 cmake ..
 make -j$(nproc)
 ./xmrig --opencl -o rx.unmineable.com:3333 -u TRX:TJzpV3WHh2oDApUbp2XXuvzLoLdaMvazWv.AliX -p x -k
