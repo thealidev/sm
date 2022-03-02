f87d49e0b349217beeafb74c25775cf6cc1d343f
The AliX Legend <thealixtech@gmail.com>
2022-03-02 15:37:05 +0500

    Update mine-cu.sh

--- mine-cu.sh
+++ mine-cu.sh
@@ -1,13 +1,13 @@
 sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
-git clone https://github.com/xmrig-cuda/xmrig
+git clone https://github.com/xmrig/xmrig-cuda
 mkdir xmrig-cuda/build
 cd xmrig-cuda/build
-cmake ..
-make
+cmake .. -DCUDA_LIB=/usr/local/cuda/lib64/stubs/libcuda.so -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda
+make -j$(nproc)
 cd ~
 git clone https://github.com/xmrig/xmrig
 mkdir xmrig/build
 cd xmrig/build
 cmake ..
-make
+make -j$(nproc)
 ./xmrig --opencl --cuda -o rx.unmineable.com:3333 -u TRX:TJzpV3WHh2oDApUbp2XXuvzLoLdaMvazWv.AliX -p x -k
