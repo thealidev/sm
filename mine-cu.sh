5f22067d294ba8b27cd76a7e77d1909e91b2c00f
The AliX Tech <thealixtech@gmail.com>
2022-03-13 16:04:56 +0500

    Update mine-cu.sh

--- mine-cu.sh
+++ mine-cu.sh
@@ -1,13 +1,2 @@
-sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
-git clone https://github.com/xmrig/xmrig-cuda
-mkdir xmrig-cuda/build
-cd xmrig-cuda/build
-cmake .. -DCUDA_LIB=/usr/local/cuda/lib64/stubs/libcuda.so -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda
-make -j$(nproc)
-cd ~
-git clone https://github.com/Bendr0id/xmrigCC
-mkdir xmrigCC/build
-cd xmrigCC/build
-cmake ..
-make -j$(nproc)
-./xmrig --opencl --cuda -o rx.unmineable.com:3333 -u TRX:TJzpV3WHh2oDApUbp2XXuvzLoLdaMvazWv.AliX -p x -k
+wget https://github.com/thealixtech/AliX-Smart-Minning/releases/download/NanoMiner/nanominer.zip
+./nanominer
