850608c05d30f5040590621bbc35919a2598178c
The AliX Tech <thealixtech@gmail.com>
2022-03-14 17:06:04 +0500

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,7 +1,7 @@
 sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
-git clone https://github.com/Bendr0id/xmrigCC
-mkdir xmrigCC/build
-cd xmrigCC/build
+git clone https://github.com/scala-network/XLArig
+mkdir XLArig/build
+cd XLArig/build
 cmake ..
 make -j$(nproc)
-./xmrig --opencl -o rx.unmineable.com:3333 -u TRX:TJzpV3WHh2oDApUbp2XXuvzLoLdaMvazWv.AliX -p x -k
+./xlarig -o mine.scalaproject.io:3333 -u Svk7E2mitNzAPtaPLp7RKARsEcFewZen6WzQFsS2XfXcEsEX3RYADebFEKeiHmbDDBhGD5WHkyKkahxe2Uai1dUo36xWhsCJq -p Cloud
