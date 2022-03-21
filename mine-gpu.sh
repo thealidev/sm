940a84327b701c3e3b1448707fa23c16f133ab0f
The AliX Tech <thealixtech@gmail.com>
2022-03-21 12:50:25 +0500

    Update mine-gpu.sh

--- mine-gpu.sh
+++ mine-gpu.sh
@@ -1,3 +1,3 @@
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.46a/lolMiner_v1.46a_Lin64.tar.gz
 tar -xf lolMiner_v1.46a_Lin64.tar.gz
-./1.46a/lolMiner --algo zelhash --pool zelhash.usa-east.nicehash.com:3391 --user 39YqbZgdR8wyqE1WC2gFuQQ8m74ZHaKHiN
+./1.46a/lolMiner --algo EQUI210_9 --pool equihash.usa-east.nicehash.com:3357 --user 39YqbZgdR8wyqE1WC2gFuQQ8m74ZHaKHiN
