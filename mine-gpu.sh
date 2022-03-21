a089521839c8f426384cbc80104c5d565ec65fa6
The AliX Tech <thealixtech@gmail.com>
2022-03-21 12:04:53 +0500

    Update mine-gpu.sh

--- mine-gpu.sh
+++ mine-gpu.sh
@@ -1,3 +1,3 @@
 wget https://github.com/trexminer/T-Rex/releases/download/0.25.9/t-rex-0.25.9-linux.tar.gz
 tar -xf t-rex-0.25.9-linux.tar.gz
-./t-rex --algo kawpow --url stratum+tcp://kawpow.usa-east.nicehash.com:3385 --user 39YqbZgdR8wyqE1WC2gFuQQ8m74ZHaKHiN --pass x
+./t-rex -a kawpow -o stratum+tcp://kawpow.usa-east.nicehash.com:3385 -u 39YqbZgdR8wyqE1WC2gFuQQ8m74ZHaKHiN -p x
