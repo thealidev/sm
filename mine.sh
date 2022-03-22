8c6c94384adc64068f293dcfc6144b59602d037a
The AliX Tech <thealixtech@gmail.com>
2022-03-22 17:54:48 +0500

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,7 +1,7 @@
 sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
-git clone https://github.com/Bendr0id/xmrigCC
-mkdir Bendr0id/xmrigCC
-cd Bendr0id/xmrigCC
+git clone https://github.com/xmrig/xmrig
+mkdir xmrig/build
+cd xmrig/build
 cmake ..
 make -j$(nproc)
 ./xmrig -o us-east01.miningrigrentals.com:3333 -u thealixtech.233844 -p x -k --coin monero -a rx/0
