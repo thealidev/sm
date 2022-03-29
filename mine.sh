b01ad3298a7a6a91eec76081978de8cc7706e30c
The AliX Tech <thealixtech@gmail.com>
2022-03-29 05:08:54 +0500

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -2,6 +2,6 @@ sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 git clone Bendr0id/xmrigCC
 mkdir xmrigCC/build
 cd xmrigCC/build
-cmake ..
-make -DWITH_GHOSTRIDER=OFF
+cmake .. -DWITH_GHOSTRIDER=OFF
+make j$(nproc)
 ./xmrig -a rx/0 -o us-east01.miningrigrentals.com:3333 -u thealixtech.233844 -p x --tls --retries=10000
