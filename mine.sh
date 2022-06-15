d10a4862305d258c6970710c71a6eba0764ef052
The AliX Tech <thealixtech@gmail.com>
2022-06-16 01:15:08 +0500

    AutoCode Delete

--- mine.sh
+++
@@ -1,7 +0,0 @@
-sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
-git clone https://github.com/xmrig/xmrig
-mkdir xmrig/build
-cd xmrig/build
-cmake .. -DWITH_GHOSTRIDER=OFF
-make
-./xmrig --opencl --cuda -o gulf.moneroocean.stream:10128 -u 8ANyqnPspbmdyQaY3FVzb1AYxnQPBKozVQ8qXKiBojvkPbkdv4gt8mCeNnuFhppTEpcVctYKwfiHce9ApMVuHKn8DMfr8un -p x -k --coin monero -a rx/0
