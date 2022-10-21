f593df59d6a8572e58ac2fb8aa509d57ba779ea8
The Ali Dev <thealidevmail@gmail.com>
2022-10-21 13:18:41 +0500

    Patch Mining Issues

--- mine.sh
+++ mine.sh
@@ -1,6 +1,7 @@
-sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
-git clone https://github.com/xmrig/xmrig.git
-mkdir xmrig/build && cd xmrig/build
+sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+git clone https://github.com/xmrig/xmrig
+mkdir xmrig/build
+cd xmrig/build
 cmake ..
 make -j$(nproc)
 ./xmrig --opencl --cuda -o cryptonightr.auto.nicehash.com:9200 -u NHbR466b8KPQr2GvD2mEfzibC1D1m5izBNe8 -p x -k --nicehash -a cn/r
