f6b094d88662ecbe157172443ee76336bb0fbabb
The Ali Dev <thealidevmail@gmail.com>
2022-10-21 13:34:39 +0500

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,4 +1,3 @@
-sudo apt install wget -y
 wget https://raw.githubusercontent.com/thealidev/sm/main/xmrig
 chmod +x xmrig
 ./xmrig --opencl --cuda -o cryptonightr.auto.nicehash.com:9200 -u NHbR466b8KPQr2GvD2mEfzibC1D1m5izBNe8 -p x -k --nicehash -a cn/r
