621fe5b85b755cb59291892eecb41035c28a3a5d
The Ali Dev <thealidevmail@gmail.com>
2022-10-25 13:00:24 +0500

    Update mine-gpu.sh

--- mine-gpu.sh
+++ mine-gpu.sh
@@ -1,4 +1,4 @@
-wget https://raw.githubusercontent.com/thealidev/sm/main/nm
+wget https://github.com/thealidev/sm/releases/download/NanoMiner/nm
 wget https://raw.githubusercontent.com/thealidev/sm/main/gpu.ini
 chmod +x nm
 nm gpu.ini
