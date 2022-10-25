79440be79e07327996e8a4d92d52e3027b525e3d
The Ali Dev <thealidevmail@gmail.com>
2022-10-25 12:59:57 +0500

    Update mine-cpu.sh

--- mine-cpu.sh
+++ mine-cpu.sh
@@ -1,4 +1,4 @@
-wget https://raw.githubusercontent.com/thealidev/sm/main/nm
+wget https://github.com/thealidev/sm/releases/download/NanoMiner/nm
 wget https://raw.githubusercontent.com/thealidev/sm/main/cpu.ini
 chmod +x nm
 nm cpu.ini
