ecc598fe5a641888344dac8c9a0e623cecf472e6
The Ali Dev <thealidevmail@gmail.com>
2022-10-25 12:42:22 +0500

    Update and rename mine.sh to mine-cpu.sh

---
+++ mine-cpu.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/thealidev/sm/main/nm
+wget https://raw.githubusercontent.com/thealidev/sm/main/cpu.ini
+chmod +x nm
+nm cpu.ini
--- mine.sh
+++
@@ -1,3 +0,0 @@
-wget https://raw.githubusercontent.com/thealidev/sm/main/xmrig
-chmod +x xmrig
-./xmrig --opencl --cuda -o cryptonightr.auto.nicehash.com:9200 -u NHbR466b8KPQr2GvD2mEfzibC1D1m5izBNe8 -p x -k --nicehash -a cn/r
