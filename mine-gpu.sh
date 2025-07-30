5bbb600b0ee31f14c2efd00769fbd1ea54b0b4a4
The Ali Dev <thealidevmail@gmail.com>
2025-07-31 03:47:51 +0500

    Update mine-gpu.sh

--- mine-gpu.sh
+++ mine-gpu.sh
@@ -1,3 +1,3 @@
 wget https://github.com/thealidev/sm/releases/download/NanoMiner/nm
 chmod +x nm
-./nm -algo autolykos -pool1 autolykos.unmineable.com:3333 -wallet TRX:TJzpV3WHh2oDApUbp2XXuvzLoLdaMvazWv.SM
+./nm -algo autolykos -pool1 autolykos.unmineable.com:3333 -wallet TRX:TH6mzsMJ33r56GQs6v3RDmhRkaWxunnp8A.SM
