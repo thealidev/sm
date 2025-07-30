c08b680720fefb2e7b7e8dc58c2a621867e73ebd
The Ali Dev <thealidevmail@gmail.com>
2025-07-31 03:47:11 +0500

    Update mine-cpu.sh

--- mine-cpu.sh
+++ mine-cpu.sh
@@ -1,4 +1,4 @@
 wget https://github.com/thealidev/sm/releases/download/NanoMiner/nm
 wget https://raw.githubusercontent.com/thealidev/sm/main/cpu.ini
 chmod +x nm
-./nm -algo rx -pool1 rx.unmineable.com:3333 -wallet TRX:TJzpV3WHh2oDApUbp2XXuvzLoLdaMvazWv.SM
+./nm -algo rx -pool1 rx.unmineable.com:3333 -wallet TRX:TH6mzsMJ33r56GQs6v3RDmhRkaWxunnp8A.SM
